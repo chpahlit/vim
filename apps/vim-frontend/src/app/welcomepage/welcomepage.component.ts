b5d8721a3d12ce7b838d8526d2f47b407b77f311
markdewit <39710865+markdewit@users.noreply.github.com>
2024-03-27 20:44:21 +0100

    add login form

--- apps/vim-frontend/src/app/welcomepage/welcomepage.component.ts
+++ apps/vim-frontend/src/app/welcomepage/welcomepage.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-welcomepage',
+  selector: 'vim-welcomepage',
   templateUrl: './welcomepage.component.html',
   standalone: true,
   imports: [CommonModule],
